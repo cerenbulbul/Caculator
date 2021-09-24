d101e042090fbbba11fc5ca46db5e5fa96220ddb
cerenbulbul <cerenbulbul27@gmail.com>
2021-09-24 09:50:56 +0200

    calculator and history card design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState} from 'react'
 
 function App() {
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button>{i}</button>
+      )
+    }
+    return digits;
+  }
+
+  const showHistory = () => {
+    const digits = [];
+
+    for (let i = 0; i < 10; i++) {
+      digits.push(
+        <p>{i}</p>
+      )
+    }
+    return digits;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span>
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+
+      <div className="history">
+        <h2>History</h2>
+
+        <div className="history-show">
+          {showHistory()}
+        </div>
+
+      </div>
     </div>
   );
 }
